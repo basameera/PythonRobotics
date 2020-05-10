4384a5cb6ed5fed1fc602827aa4f25568d5d80cb
basameera <basameera@protonmail.com>
2020-05-10 16:53:26 +0200

    BFB mesure, motion models are ready

--- MyCode/Bayes Filter/bayes_filter_viz.py
+++ MyCode/Bayes Filter/bayes_filter_viz.py
@@ -6,7 +6,6 @@ import matplotlib.image as mpimg
 from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
 
 
-
 class BayesFilterBoat(object):
 
     def __init__(self):
@@ -18,22 +17,37 @@ class BayesFilterBoat(object):
         self._draw_main_window()
 
     def _draw_main_window(self):
-        # self.fig, self.axs = plt.subplots(1, 3, figsize=(12, 4))
-
-        self.fig = plt.figure(figsize=(10, 8))
+        self.fig = plt.figure(figsize=(10, 8), constrained_layout=False)
         gs = self.fig.add_gridspec(2, 2)
 
         # sim axis
         self.ax_sim = self.fig.add_subplot(gs[0, :])
         self.ax_sim.set_title('Simulation')
+        self.set_ylim(self.ax_sim)
+        self.ax_sim.set_xlim(-1, 11)
 
         # Mesurement model
         self.ax_me = self.fig.add_subplot(gs[1, 0])
-        self.ax_me.set_title('Mesurement Model')
+
+        self.me_y = np.ones(shape=(3,))/3
+        self.me_x = np.arange(len(self.me_y)) - 1
+
+        self.ax_me.set_title(r'Mesurement model   $p(Z_t|X_t)$')
+        bar_plot(self.ax_me, self.me_x, self.me_y, color='r')
+        self.set_ylim(self.ax_me)
 
         # Motion model
         self.ax_mo = self.fig.add_subplot(gs[1, 1])
-        self.ax_mo.set_title('Motion Model')
+        self.ax_mo.set_title(r'Motion model   $p(X_{t+1}|X_t)$')
+
+        self.mo_p = [0, 0.75, 0.25]
+        self.mo_y = np.array(self.mo_p)
+        self.mo_x = np.arange(len(self.mo_y))
+
+        bar_plot(self.ax_mo, self.mo_x, self.mo_y, color='b')
+        self.ax_mo.axvline(0.25, c='k', ls='--')
+        self.ax_mo.axvline(-0.25, c='k', ls='--')
+        self.set_ylim(self.ax_mo)
 
         line, = self.ax_sim.plot([0], [0])
         self.cid = line.figure.canvas.mpl_connect('button_press_event', self)
@@ -41,6 +55,11 @@ class BayesFilterBoat(object):
         self.xs = list(line.get_xdata())
         self.ys = list(line.get_ydata())
         plt.show()
+
+    def set_ylim(self, ax):
+        ax.set_ylim(0, 1)
+        ax.set_axisbelow(True)
+        ax.grid(ls='--')
 
     def __call__(self, event):
         # print('click', event)
