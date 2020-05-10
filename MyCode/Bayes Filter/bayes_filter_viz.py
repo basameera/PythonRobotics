4d0b0eaa3247ce222312b6f2919a13d8e67feb44
basameera <basameera@protonmail.com>
2020-05-10 18:24:30 +0200

    mesurement part ready

--- MyCode/Bayes Filter/bayes_filter_viz.py
+++ MyCode/Bayes Filter/bayes_filter_viz.py
@@ -9,9 +9,19 @@ from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationB
 class BayesFilterBoat(object):
 
     def __init__(self):
+        np.random.seed(123)
         self.boat_img = mpimg.imread('/home/sameera/Downloads/sailboat.png')
         self.click_counter = 0
         self.simulation_scope = 10
+        self.boat_pos = 0
+
+        self.SS_INIT = 0  # INIT
+        self.SS_READ_ME = 1  # read mesurement
+        self.SS_FINISH_ME = 2  # finish mesurement
+        self.SS_MOTION = 3  # motion
+
+        self.sim_state = self.SS_READ_ME
+        self.run = True
 
     def main(self):
         self._draw_main_window()
@@ -23,19 +33,20 @@ class BayesFilterBoat(object):
 
         # sim axis
         self.ax_sim = self.fig.add_subplot(gs[0, :])
-        self.ax_sim.set_title('Simulation', fontsize=10)
 
         # Initial p(x)
-        self.sim_p = [0.6, 0.5]
-        self.sim_y = np.array(self.sim_p)
+        self.sim_p = [1, 2, 1]
+        self.sim_y = norm(self.sim_p)
         self.sim_y = np.concatenate(
             (self.sim_y, np.zeros(self.simulation_scope-len(self.sim_p),)))
         self.sim_x = np.arange(self.simulation_scope)
 
+        self.boat_pos = np.argmax(self.sim_y)
+        print('boat pos:', self.boat_pos)
+
         bar_plot(self.ax_sim, self.sim_x, self.sim_y,
                  color='tab:blue', label='Init')
-        self.draw_boat(self.ax_sim)
-
+        self.draw_boat(self.ax_sim, self.boat_pos)
         self.setup_ax_sim()
 
         # Mesurement model
@@ -70,7 +81,7 @@ class BayesFilterBoat(object):
 
     def draw_boat(self, ax, x=0):
         imagebox = OffsetImage(self.boat_img, zoom=0.2)
-        ab = AnnotationBbox(imagebox, (x, 0.8), frameon=False)
+        ab = AnnotationBbox(imagebox, (x, 1.0), frameon=False)
         ax.add_artist(ab)
 
     def set_ylim(self, ax):
@@ -80,39 +91,69 @@ class BayesFilterBoat(object):
 
     def get_color_label(self):
         if self.click_counter % 2 == 0:
-            color = 'tab:blue'
+            color = 'tab:orange'
             label = 'Mesurement'
         else:
-            color = 'tab:orange'
+            color = 'tab:blue'
             label = 'Motion'
         return color, label
 
     def setup_ax_sim(self):
         self.ax_sim.set_xlim(-1, 11)
         self.ax_sim.legend()
-        self.set_ylim(self.ax_sim)
+        self.ax_sim.set_ylim(0, 1.2)
+        self.ax_sim.set_axisbelow(True)
+        self.ax_sim.grid(ls='--')
+        self.ax_sim.set_title('Simulation', fontsize=10)
+
+    def read_mesurement(self):
+        """NOTE: This need to change for better sim.
+        At the moment, same mesurement model was used for reading mesurement.
+
+        This give how much of a position change does happen from the current boat position
+
+        """
+        return np.random.choice(self.me_x, p=self.me_y)
+
+    def get_me_prob(self, pred_boat_pos):
+        new_me_y = np.zeros_like(self.sim_y)
+        for n, me_x in enumerate(self.me_x):
+            idx = pred_boat_pos + me_x
+            if idx >= 0 and idx < self.simulation_scope:
+                new_me_y[idx] = self.me_y[n]
+        return new_me_y
 
     def __call__(self, event):
 
-        self.click_counter += 1
         if event.key == 'escape':
             exit(0)
         elif event.key == 'right':
             ax = self.ax_sim
             ax.clear()
-            data = np.random.random((10,))
-            c, l = self.get_color_label()
-            p = [0.5, 0.5, 1]
-            y = norm(p)
-            y = np.concatenate((y, np.zeros(self.simulation_scope-len(p),)))
-            x = np.arange(self.simulation_scope)
 
-            bar_plot(ax, x, y, color=c, label=l)
-            self.draw_boat(ax, 2)
+            if self.click_counter % 2 == 0:
+                # read ME
+                # add the positoin change to boat position to get the new boat position
+                pred_boat_pos = self.read_mesurement() + self.boat_pos
+                print('predicted boat pos:', pred_boat_pos)
+                Z = self.get_me_prob(pred_boat_pos)
+                # multiply position prob. with sensor pos prob. to get the predicted boat prob.
+                pred_sim_y = self.sim_y * Z
+                # normamlize
+                self.sim_y = norm(pred_sim_y)
+                self.boat_pos = np.argmax(self.sim_y)
+                c, l = self.get_color_label()
+                bar_plot(self.ax_sim, self.sim_x, self.sim_y,
+                         color=c, label=l)
+                self.draw_boat(self.ax_sim, self.boat_pos)
+                self.setup_ax_sim()
 
-            self.setup_ax_sim()
+            else:
+                qwe = 'Motion'
 
             self.fig.canvas.draw()
+
+        self.click_counter += 1
 
 
 if __name__ == "__main__":
