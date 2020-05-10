8533b007bde3464990ce8168531884c302951a5c
basameera <basameera@protonmail.com>
2020-05-10 17:20:18 +0200

    sim ready for logic

--- MyCode/Bayes Filter/bayes_filter_viz.py
+++ MyCode/Bayes Filter/bayes_filter_viz.py
@@ -18,13 +18,25 @@ class BayesFilterBoat(object):
 
     def _draw_main_window(self):
         self.fig = plt.figure(figsize=(10, 8), constrained_layout=False)
+        self.fig.suptitle('Bayes Filter - Boat simulation', fontsize=13)
         gs = self.fig.add_gridspec(2, 2)
 
         # sim axis
         self.ax_sim = self.fig.add_subplot(gs[0, :])
-        self.ax_sim.set_title('Simulation')
-        self.set_ylim(self.ax_sim)
-        self.ax_sim.set_xlim(-1, 11)
+        self.ax_sim.set_title('Simulation', fontsize=10)
+
+        # Initial p(x)
+        self.sim_p = [0.6, 0.5]
+        self.sim_y = np.array(self.sim_p)
+        self.sim_y = np.concatenate(
+            (self.sim_y, np.zeros(self.simulation_scope-len(self.sim_p),)))
+        self.sim_x = np.arange(self.simulation_scope)
+
+        bar_plot(self.ax_sim, self.sim_x, self.sim_y,
+                 color='tab:blue', label='Init')
+        self.draw_boat(self.ax_sim)
+
+        self.setup_ax_sim()
 
         # Mesurement model
         self.ax_me = self.fig.add_subplot(gs[1, 0])
@@ -32,13 +44,13 @@ class BayesFilterBoat(object):
         self.me_y = np.ones(shape=(3,))/3
         self.me_x = np.arange(len(self.me_y)) - 1
 
-        self.ax_me.set_title(r'Mesurement model   $p(Z_t|X_t)$')
+        self.ax_me.set_title(r'Mesurement model   $p(Z_t|X_t)$', fontsize=10)
         bar_plot(self.ax_me, self.me_x, self.me_y, color='r')
         self.set_ylim(self.ax_me)
 
         # Motion model
         self.ax_mo = self.fig.add_subplot(gs[1, 1])
-        self.ax_mo.set_title(r'Motion model   $p(X_{t+1}|X_t)$')
+        self.ax_mo.set_title(r'Motion model   $p(X_{t+1}|X_t)$', fontsize=10)
 
         self.mo_p = [0, 0.75, 0.25]
         self.mo_y = np.array(self.mo_p)
@@ -50,25 +62,57 @@ class BayesFilterBoat(object):
         self.set_ylim(self.ax_mo)
 
         line, = self.ax_sim.plot([0], [0])
-        self.cid = line.figure.canvas.mpl_connect('button_press_event', self)
+        self.cid = line.figure.canvas.mpl_connect('key_release_event', self)
         self.line = line
         self.xs = list(line.get_xdata())
         self.ys = list(line.get_ydata())
         plt.show()
+
+    def draw_boat(self, ax, x=0):
+        imagebox = OffsetImage(self.boat_img, zoom=0.2)
+        ab = AnnotationBbox(imagebox, (x, 0.8), frameon=False)
+        ax.add_artist(ab)
 
     def set_ylim(self, ax):
         ax.set_ylim(0, 1)
         ax.set_axisbelow(True)
         ax.grid(ls='--')
 
+    def get_color_label(self):
+        if self.click_counter % 2 == 0:
+            color = 'tab:blue'
+            label = 'Mesurement'
+        else:
+            color = 'tab:orange'
+            label = 'Motion'
+        return color, label
+
+    def setup_ax_sim(self):
+        self.ax_sim.set_xlim(-1, 11)
+        self.ax_sim.legend()
+        self.set_ylim(self.ax_sim)
+
     def __call__(self, event):
-        # print('click', event)
-        if event.inaxes != self.line.axes:
-            return
-        self.xs.append(event.xdata)
-        self.ys.append(event.ydata)
-        self.line.set_data(self.xs, self.ys)
-        self.line.figure.canvas.draw()
+
+        self.click_counter += 1
+        if event.key == 'escape':
+            exit(0)
+        elif event.key == 'right':
+            ax = self.ax_sim
+            ax.clear()
+            data = np.random.random((10,))
+            c, l = self.get_color_label()
+            p = [0.5, 0.5, 1]
+            y = norm(p)
+            y = np.concatenate((y, np.zeros(self.simulation_scope-len(p),)))
+            x = np.arange(self.simulation_scope)
+
+            bar_plot(ax, x, y, color=c, label=l)
+            self.draw_boat(ax, 2)
+
+            self.setup_ax_sim()
+
+            self.fig.canvas.draw()
 
 
 if __name__ == "__main__":
